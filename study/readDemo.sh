a3b2ba4fd0a830ace5317dc47b506b9d4b670713
liukai <869896679@qq.com>
2020-08-14 10:11:19 +0800

    read命令

--- study/readDemo.sh
+++ study/readDemo.sh
@@ -16,4 +16,38 @@ while read myline;do
     echo $myline
 done < $filename
 
-#-t参数 
+#-t参数,设置超时的秒数
+echo -n "输入一些文本 >"
+if read -t 3 response;then
+    echo "用户已经输入了,内容为:$response"
+else
+    echo "用户没有输入"
+fi
+
+#-p参数,指定用户输入的提示信息
+read -p "enter one or more values >"
+echo "REPLY = $REPLY"
+
+#-a参数,将用户的输入赋值给一个数组，索引从0开始
+read -p "input people names >" -a people
+echo ${people[2]}
+
+#-s参数,使用户的输入不显示在屏幕上
+read -p "input your passport >" -s ps
+echo -e "\nps:$ps"
+
+FILE=/etc/passwd
+read -p "enter a username >" username
+info=$(grep "^$username:" $FILE)
+if [ -n "$info" ];then
+    IFS=":" read user pw uid gid name home shell <<< "$info"
+    echo "User = '$user'"
+    echo "UID = '$uid'"
+    echo "GID = '$gid'"
+    echo "Full Name = '$name'"
+    echo "Home Dir = '$home'"
+    echo "Shell = '$shell'"
+else
+    echo "No such user '$username'" >&2
+    exit 1
+fi
